773b23e22b88f656e88175981c0813fca3448613
nukopy <nukopy@gmail.com>
2024-01-21 23:48:02 +0900

    feat: impl sample test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn test_add() {
+    // test content: add(1, 2) == 3
+    // given (prerequisites):
+    let expected = 3;
+
+    // when (operation):
+    let actual = add(1, 2);
+
+    // then (expected results):
+    assert_eq!(actual, expected, "1 + 2 should be 3");
+}
